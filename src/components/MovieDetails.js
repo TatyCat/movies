7b6a811fc84da42aa16586d97d1e69c3530f757e
TatyCat <williams.tatyanna@gmail.com>
2019-03-04 14:32:57 -0500

    Testing random movie font size

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -10,7 +10,7 @@ class MovieDetails extends Component {
         <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2//yV9G1nO6YxEWzXblqMUFoS19R9v.jpg"></img>
         <h3>Released: Thu Feb 21 2019</h3>
         <h2>Voter Average: 10/10</h2>
-        <figcaption> Summary goes here </figcaption>
+        <figcaption> When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past. </figcaption>
 
         <h2>Starring Cast</h2>
         <ul>
