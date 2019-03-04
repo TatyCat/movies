ea693c46a724d322f176d8d8d496fb8a242d96e5
Angie-FL <angie.pachcinski@gmail.com>
2019-03-03 21:19:45 -0500

    Created Movie Outline, movie details will go here

---
+++ src/components/MovieOutline.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class MovieOutline extends Component {
+  render() {
+    return (
+      <div>
+        <section>
+          <figure>
+            {/* <Img src= images goes here'/> */}
+            <h1>Title goes here</h1>
+            <h2>Rating goes here</h2>
+            <h3>Release date goes here</h3>
+            <figcaption> Summary goes here </figcaption>
+          </figure>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default MovieOutline
