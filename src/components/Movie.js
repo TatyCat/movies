2f445b738d51fd0098e14774676b2c64748b9d8c
TatyCat <williams.tatyanna@gmail.com>
2019-03-04 11:21:52 -0500

    Tweaking CSS on home page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -36,7 +36,7 @@ class Movie extends Component {
       <article>
         <h2>{this.props.title}</h2>
         <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${this.props.urlImg}`}></img>
-        <p>{this.props.dateMovie}</p>
+        <p>Released: {this.props.dateMovie}</p>
         <p>Voter Average: {this.props.rating}/10</p>
         <p>{this.props.detail}</p>
         <a href="#">More Information</a>
