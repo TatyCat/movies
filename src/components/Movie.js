4b791316cf4ee3a2c71ef2186d960bbc7bfb47ae
Juan <juanxsalazar21@gmail.com>
2019-03-04 08:07:41 -0500

    commented out most codes and put in the code that will show movies currently playing. have totly playing

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,80 +1,108 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-
-//API key is: c7519dda6166314cd0297ca58edc6207
 
 class Movie extends Component {
+
   render() {
     return (
       <main>
         <article>
-
-          <h2>Cold Pursuit</h2>
-          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
-          <p>83%</p>
-          <p>Feburary 8, 2019</p>
-          <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
+          <h2>{this.props.title}</h2>
+          <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${this.props.urlImg}`}
+           alt={this.props.title} />
+          <p>{this.props.dateMovie}</p>
+          <p>Voter Average: {this.props.rating}/10</p>
+          <p>{this.props.detail}</p>
           <a href="#">More Information</a>
-
-        </article>
-
-        <article>
-          <h2>Glass</h2>
-          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/lvjscO8wmpEbIfOEZi92Je8Ktlg.jpg'></img>
-          <p>83%</p>
-          <p>Feburary 8, 2019</p>
-          <p>In a series of escalating encounters, security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.</p>
-          <a href="#">More Information</a>
-
-        </article>
-
-        <article>
-
-          <h2>Cold Pursuit</h2>
-          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
-          <p>83%</p>
-          <p>Feburary 8, 2019</p>
-          <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
-          <a href="#">More Information</a>
-
-        </article>
-
-        <article>
-
-          <h2>Cold Pursuit</h2>
-          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
-          <p>83%</p>
-          <p>Feburary 8, 2019</p>
-          <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
-          <a href="#">More Information</a>
-
-        </article>
-
-        <article>
-
-          <h2>The Favourite</h2>
-          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/cwBq0onfmeilU5xgqNNjJAMPfpw.jpg'></img>
-          <p>91%</p>
-          <p>Feburary 8, 2019</p>
-          <p>England, early 18th century. The close relationship between Queen Anne and Sarah Churchill is threatened by the arrival of Sarah's cousin, Abigail Hill, resulting in a bitter rivalry between the two cousins to be the Queen's favourite.</p>
-          <a href="#">More Information</a>
-
-        </article>
-
-
-        <article>
-
-          <h2>Serenity</h2>
-          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hgWAcic93phg4DOuQ8NrsgQWiqu.jpg'></img>
-          <p>31%</p>
-          <p>Feburary 8, 2019</p>
-          <p>Baker Dill is a fishing boat captain leading tours off a tranquil, tropical enclave called Plymouth Island. His quiet life is shattered, however, when his ex-wife Karen tracks him down with a desperate plea for help.</p>
-          <a href="#">More Information</a>
-
         </article>
       </main>
+      
     )
   }
 }
 
 export default Movie
+
+
+
+
+
+// import React, { Component } from 'react'
+// import axios from 'axios'
+
+// //API key is: c7519dda6166314cd0297ca58edc6207
+
+// class Movie extends Component {
+//   render() {
+//     return (
+//       <main>
+//         <article>
+
+//           <h2>Cold Pursuit</h2>
+//           <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
+//           <p>83%</p>
+//           <p>Feburary 8, 2019</p>
+//           <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
+//           <a href="#">More Information</a>
+
+//         </article>
+
+//         <article>
+//           <h2>Glass</h2>
+//           <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/lvjscO8wmpEbIfOEZi92Je8Ktlg.jpg'></img>
+//           <p>83%</p>
+//           <p>Feburary 8, 2019</p>
+//           <p>In a series of escalating encounters, security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.</p>
+//           <a href="#">More Information</a>
+
+//         </article>
+
+//         <article>
+
+//           <h2>Cold Pursuit</h2>
+//           <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
+//           <p>83%</p>
+//           <p>Feburary 8, 2019</p>
+//           <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
+//           <a href="#">More Information</a>
+
+//         </article>
+
+//         <article>
+
+//           <h2>Cold Pursuit</h2>
+//           <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
+//           <p>83%</p>
+//           <p>Feburary 8, 2019</p>
+//           <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
+//           <a href="#">More Information</a>
+
+//         </article>
+
+//         <article>
+
+//           <h2>The Favourite</h2>
+//           <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/cwBq0onfmeilU5xgqNNjJAMPfpw.jpg'></img>
+//           <p>91%</p>
+//           <p>Feburary 8, 2019</p>
+//           <p>England, early 18th century. The close relationship between Queen Anne and Sarah Churchill is threatened by the arrival of Sarah's cousin, Abigail Hill, resulting in a bitter rivalry between the two cousins to be the Queen's favourite.</p>
+//           <a href="#">More Information</a>
+
+//         </article>
+
+
+//         <article>
+
+//           <h2>Serenity</h2>
+//           <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hgWAcic93phg4DOuQ8NrsgQWiqu.jpg'></img>
+//           <p>31%</p>
+//           <p>Feburary 8, 2019</p>
+//           <p>Baker Dill is a fishing boat captain leading tours off a tranquil, tropical enclave called Plymouth Island. His quiet life is shattered, however, when his ex-wife Karen tracks him down with a desperate plea for help.</p>
+//           <a href="#">More Information</a>
+
+//         </article>
+//       </main>
+//     )
+//   }
+// }
+
+// export default Movie
