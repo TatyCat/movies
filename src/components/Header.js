70144741a750fa54d1c27ae4c5d0dd82389d7f8b
TatyCat <williams.tatyanna@gmail.com>
2019-03-04 10:45:16 -0500

    Fixed Home Page. Need to adjust size of poster images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
-// import React, { Component } from 'react'
+import React, { Component } from 'react'
 
-// class Header extends Component {
-//   render() {
-//     return (
-//       <header>
-//         <h1>Movies Now Showing</h1>
-//       </header>
-//     )
-//   }
-// }
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Movies Now Showing</h1>
+      </header>
+    )
+  }
+}
 
-// export default Header
+export default Header
--- src/components/MoviesNowPlaying.js
+++ src/components/MoviesNowPlaying.js
@@ -1,18 +1,16 @@
-// import React, { Component } from 'react'
-// import Movie from './Movie'
-// import Header from './Header'
-// import axios from 'axios'
+import React, { Component } from 'react'
+import Movie from './Movie'
 
-// //API key is: c7519dda6166314cd0297ca58edc6207
+//API key is: c7519dda6166314cd0297ca58edc6207
 
-// class MoviesNowPlaying extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <Movie />
-//       </div>
-//     )
-//   }
-// }
+class MoviesNowPlaying extends Component {
+  render() {
+    return (
+      <>
+        <Movie />
+      </>
+    )
+  }
+}
 
-// export default MoviesNowPlaying
+export default MoviesNowPlaying
