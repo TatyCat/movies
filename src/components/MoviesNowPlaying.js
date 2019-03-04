e9c7deca808eb04a217ac374712c9a6b6fabd895
Juan <juanxsalazar21@gmail.com>
2019-03-04 08:14:33 -0500

    other components should be commented out now along with app js

--- src/components/MoviesNowPlaying.js
+++ src/components/MoviesNowPlaying.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import Movie from './Movie'
-import Header from './Header'
-import axios from 'axios'
+// import React, { Component } from 'react'
+// import Movie from './Movie'
+// import Header from './Header'
+// import axios from 'axios'
 
-//API key is: c7519dda6166314cd0297ca58edc6207
+// //API key is: c7519dda6166314cd0297ca58edc6207
 
-class MoviesNowPlaying extends Component {
-  render() {
-    return (
-      <div>
-        <Movie />
-      </div>
-    )
-  }
-}
+// class MoviesNowPlaying extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Movie />
+//       </div>
+//     )
+//   }
+// }
 
-export default MoviesNowPlaying
+// export default MoviesNowPlaying
