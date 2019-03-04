9951dafd9e78e250a160c931f7e4360b66e5a760
TatyCat <williams.tatyanna@gmail.com>
2019-03-03 19:19:30 -0500

    Updated the more information button

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -24,7 +24,9 @@ class Movie extends Component {
               <p>Cold Pursuit</p>
               <p>83%</p>
               <p>Feburary 8, 2019</p>
-              <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
+              <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…
+                h soon becomes ah soon becomes ah soon becomes ah soon becomes ah soon becomes a done
+              </p>
               <a href="#">More Information</a>
             </section>
           </div>
