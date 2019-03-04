192e03cab377ede15334060755cc1ef6679bdb4f
TatyCat <williams.tatyanna@gmail.com>
2019-03-03 23:15:21 -0500

    Revamped styling for the home page and verified mobile responsiveness

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,55 +5,69 @@ class Movie extends Component {
     return (
       <main>
         <article>
-          <div>
-            <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
-            <section>
-              <p>Cold Pursuit</p>
-              <p>83%</p>
-              <p>Feburary 8, 2019</p>
-              <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
-              <a href="#">More Information</a>
-            </section>
-          </div>
+
+          <h2>Cold Pursuit</h2>
+          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
+          <p>83%</p>
+          <p>Feburary 8, 2019</p>
+          <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
+          <a href="#">More Information</a>
+
         </article>
 
         <article>
-          <div>
-            <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
-            <section>
-              <p>Cold Pursuit</p>
-              <p>83%</p>
-              <p>Feburary 8, 2019</p>
-              <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…
-                h soon becomes ah soon becomes ah soon becomes ah soon becomes ah soon becomes a done
-              </p>
-              <a href="#">More Information</a>
-            </section>
-          </div>
+          <h2>Glass</h2>
+          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/lvjscO8wmpEbIfOEZi92Je8Ktlg.jpg'></img>
+          <p>83%</p>
+          <p>Feburary 8, 2019</p>
+          <p>In a series of escalating encounters, security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.</p>
+          <a href="#">More Information</a>
+
         </article>
+
         <article>
-          <div>
-            <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
-            <section>
-              <p>Cold Pursuit</p>
-              <p>83%</p>
-              <p>Feburary 8, 2019</p>
-              <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
-              <a href="#">More Information</a>
-            </section>
-          </div>
+
+          <h2>Cold Pursuit</h2>
+          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
+          <p>83%</p>
+          <p>Feburary 8, 2019</p>
+          <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
+          <a href="#">More Information</a>
+
         </article>
+
         <article>
-          <div>
-            <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
-            <section>
-              <p>Cold Pursuit</p>
-              <p>83%</p>
-              <p>Feburary 8, 2019</p>
-              <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
-              <a href="#">More Information</a>
-            </section>
-          </div>
+
+          <h2>Cold Pursuit</h2>
+          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hXgmWPd1SuujRZ4QnKLzrj79PAw.jpg'></img>
+          <p>83%</p>
+          <p>Feburary 8, 2019</p>
+          <p>Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord…</p>
+          <a href="#">More Information</a>
+
+        </article>
+
+        <article>
+
+          <h2>The Favourite</h2>
+          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/cwBq0onfmeilU5xgqNNjJAMPfpw.jpg'></img>
+          <p>91%</p>
+          <p>Feburary 8, 2019</p>
+          <p>England, early 18th century. The close relationship between Queen Anne and Sarah Churchill is threatened by the arrival of Sarah's cousin, Abigail Hill, resulting in a bitter rivalry between the two cousins to be the Queen's favourite.</p>
+          <a href="#">More Information</a>
+
+        </article>
+
+
+        <article>
+
+          <h2>Serenity</h2>
+          <img src='https://image.tmdb.org/t/p/w185_and_h278_bestv2/hgWAcic93phg4DOuQ8NrsgQWiqu.jpg'></img>
+          <p>31%</p>
+          <p>Feburary 8, 2019</p>
+          <p>Baker Dill is a fishing boat captain leading tours off a tranquil, tropical enclave called Plymouth Island. His quiet life is shattered, however, when his ex-wife Karen tracks him down with a desperate plea for help.</p>
+          <a href="#">More Information</a>
+
         </article>
       </main >
     )
