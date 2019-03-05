e9af59f2146b91481a9a7a9a4feea59b0ab91973
Angie-FL <angie.pachcinski@gmail.com>
2019-03-04 22:16:45 -0500

    added movie details but commented them out

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-
 import React, { Component } from 'react'
 import MovieData from './components/MovieData'
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import MovieDetails from '.components/MovieDetails'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={MovieData}/>
-     </Switch>
-     </Router>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={MovieData} />
+            {/* <Route exact path="/:movie" component={MovieDetails} /> */}
+          </Switch>
+        </Router>
       </>
     )
   }
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -3,21 +3,33 @@ import React, { Component } from 'react'
 class MovieDetails extends Component {
   render() {
     return (
-
       <section>
         <h2>Trading Paint</h2>
+        {/* <h2>{this.props.title}</h2> */}
 
-        <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2//yV9G1nO6YxEWzXblqMUFoS19R9v.jpg"></img>
+        <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2//yV9G1nO6YxEWzXblqMUFoS19R9v.jpg" />
+        {/* <img src={this.props.urlImg} alt="" /> */}
+
         <h3>Released: Thu Feb 21 2019</h3>
+        {/* <h3>{this.props.dateMovie}</h3> */}
+
         <h2>Voter Average: 10/10</h2>
-        <figcaption> When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past. </figcaption>
+        {/* <h2>{this.props.rating}</h2> */}
+
+        <figcaption>
+          {' '}
+          When Alita awakens with no memory of who she is in a future world she
+          does not recognize, she is taken in by Ido, a compassionate doctor who
+          realizes that somewhere in this abandoned cyborg shell is the heart
+          and soul of a young woman with an extraordinary past.{' '}
+        </figcaption>
+        {/* <figcaption>{this.props.details}</figcaption> */}
 
         <h2>Starring Cast</h2>
         <ul>
           <li>The cast goes here</li>
         </ul>
       </section>
-
     )
   }
 }
